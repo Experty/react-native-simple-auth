4162ea48ada74491b269d2820a843abe58f54186
Łukasz Kubica <kubicalukasz92@gmail.com>
2019-05-22 14:44:51 +0200

    twitch oAuth provider

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ import * as _facebook from './lib/providers/facebook';
 import * as _twitter from './lib/providers/twitter';
 import * as _tumblr from './lib/providers/tumblr';
 import * as _dock from './lib/providers/dock';
+import * as _twitch from './lib/providers/twitch';
 
 export const google = login(_google, platform);
 export const facebook = login(_facebook, platform);
 export const twitter = login(_twitter, platform);
 export const tumblr = login(_tumblr, platform);
 export const dock = login(_dock, platform);
+export const twitch = login(_twitch, platform);
 
 export default login(__, platform);
